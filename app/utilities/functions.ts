1b4161b1a72e3f2534e537b49c708dd631b7e07e
Kiwi <kiwipetal@kittymail.com>
2024-12-03 03:39:33 +0100

    refactor: Move project files into app folder

--- app/utilities/functions.ts
+++ app/utilities/functions.ts
@@ -1,5 +1,5 @@
 import * as StackBlur from "stackblur-canvas";
-import { createCanvas, loadImage } from "canvas";
+import { createCanvas, loadImage } from "@napi-rs/canvas";
 import {
   existsSync,
   writeFileSync,
